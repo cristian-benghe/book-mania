df8e425dc04c42a12e3f2f22feddd82feea5629b
Eduard Alex Mihai <E.A.Mihai@student.tudelft.nl>
2023-12-08 14:04:34 +0100

    disable security

--- example-microservice/src/main/java/nl/tudelft/sem/template/example/Application.java
+++ example-microservice/src/main/java/nl/tudelft/sem/template/example/Application.java
@@ -2,11 +2,12 @@ package nl.tudelft.sem.template.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
 /**
  * Example microservice application.
  */
-@SpringBootApplication
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
