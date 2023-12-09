d0af63ae1ca4f8d97d96d220065df0f3f53eb57b
Karol Sperczynski <k.sperczynski@student.tudelft.nl>
2023-12-09 22:25:28 +0100

    test: ensure that PasswordEncoder works as expected

---
+++ example-microservice/src/test/java/nl/tudelft/sem/template/example/integration/book/PasswordServiceTest.java
@@ -0,0 +1,35 @@
+package nl.tudelft.sem.template.example.integration.book;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import nl.tudelft.sem.template.example.services.PasswordService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+
+@SpringBootTest
+public class PasswordServiceTest {
+    @Autowired
+    PasswordService passwordService;
+
+    @Test
+    public void correctlyDecodesEncodedPassword() {
+        String rawPassword = "pass1233!_abc:/d'\"ui";
+        String encPassword = passwordService.passwordEncoder().encode(rawPassword);
+        boolean matches = passwordService.passwordEncoder().matches(rawPassword, encPassword);
+
+        assertTrue(matches);
+    }
+
+    @Test
+    public void doesNotMatchWrongPassword() {
+        String rawPassword = "pass1233!_abc:/d'\"ui";
+        String otherPassword = "aPassword here";
+        String encPassword = passwordService.passwordEncoder().encode(rawPassword);
+        boolean matches = passwordService.passwordEncoder().matches(otherPassword, encPassword);
+
+        assertFalse(matches);
+    }
+}
