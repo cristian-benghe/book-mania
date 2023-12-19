773a57836167d25e7e241dbbb9dcb21a1fa8f75d
Uros Gluscevic <urosgluscevic@gmail.com>
2023-12-19 11:09:15 +0100

    implemented the feature

--- example-microservice/src/main/java/nl/tudelft/sem/template/example/modules/user/FollowingType.java
+++ example-microservice/src/main/java/nl/tudelft/sem/template/example/modules/user/FollowingType.java
@@ -1,8 +1,9 @@
 package nl.tudelft.sem.template.example.modules.user;
 
 import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.Embeddable;
-import javax.persistence.OneToMany;
+import javax.persistence.ManyToMany;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -10,15 +11,25 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Embeddable
 public class FollowingType {
-    @OneToMany(targetEntity = User.class)
-    private List<Long> followedUsers;
+    @ManyToMany(cascade = CascadeType.ALL)
+    private List<User> followedUsers;
 
     /**
      * Constructor of the FollowingType.
      *
      * @param followedUsers list of userIDs of the users this account follows
      */
-    public FollowingType(List<Long> followedUsers) {
+    public FollowingType(List<User> followedUsers) {
         this.followedUsers = followedUsers;
+    }
+
+    /**
+     * Checks whether the list of followed users contains the provided ID.
+     *
+     * @param wantedUser user to check for
+     * @return whether the user with wantedID is already being followed
+     */
+    public boolean follows(User wantedUser) {
+        return this.followedUsers.contains(wantedUser);
     }
 }
