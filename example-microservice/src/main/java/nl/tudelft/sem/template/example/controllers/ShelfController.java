e3c3f755afb880c4c700410f4c15acd01ebeea84
Karol Sperczynski <k.sperczynski@student.tudelft.nl>
2024-01-16 17:46:55 +0100

    feat: integrate with Bookshelf microservice

--- example-microservice/src/main/java/nl/tudelft/sem/template/example/controllers/ShelfController.java
+++ example-microservice/src/main/java/nl/tudelft/sem/template/example/controllers/ShelfController.java
@@ -72,7 +72,7 @@ public class ShelfController {
             // send request via RestService
             HttpStatus status = restService.addToMicroservice(targetUrl, requestData);
             // check response
-            if (status == HttpStatus.OK) { // all okay on both our and other microservice's side: confirm OK
+            if (status == HttpStatus.CREATED) { // all okay on both our and other microservice's side: confirm OK
                 return ResponseEntity.status(HttpStatus.OK).body(detailsOrStatus);
             } // issue not on our side; return 500 Server Error
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
@@ -121,7 +121,7 @@ public class ShelfController {
             ManageBookShelfRequest requestData =
                 new ManageBookShelfRequest(((ManageBookShelfResponse200) detailsOrStatus).getBookID());
             // build URL
-            String targetUrl = restService.buildBookshelfURL(shelfId, userId);
+            String targetUrl = restService.buildBookshelfRemoveURL(shelfId, userId, bookId);
             // send request via RestService
             HttpStatus status = restService.removeFromMicroservice(targetUrl, requestData);
             // check response
--- example-microservice/src/main/java/nl/tudelft/sem/template/example/services/RestService.java
+++ example-microservice/src/main/java/nl/tudelft/sem/template/example/services/RestService.java
@@ -33,6 +33,17 @@ public class RestService {
             + shelfId;
     }
 
+    public String buildBookshelfRemoveURL(long shelfId, long userId, long bookId) {
+        return "http://localhost:8081/bookshelf/" +
+            shelfId +
+            "/book?userId=" +
+            userId +
+            "&books=" +
+            bookId +
+            "&bookshelfId=" +
+            shelfId;
+    }
+
     /**
      * Sends a created HTTP request to targetUrl.
      *
@@ -47,6 +58,8 @@ public class RestService {
         // create the request that will be sent
         HttpEntity<ManageBookShelfRequest> httpRequest = new HttpEntity<>(requestData, new HttpHeaders());
         // send request, and return response
+        System.out.println("THE REQUEST: " + httpRequest.toString());
+
         return new RestTemplate()
             .postForEntity(targetUrl, httpRequest, String.class)
             .getStatusCode();
@@ -64,7 +77,7 @@ public class RestService {
         ManageBookShelfRequest requestData
     ) {
         // create the request that will be sent
-        HttpEntity<ManageBookShelfRequest> httpRequest = new HttpEntity<>(requestData, new HttpHeaders());
+        HttpEntity<ManageBookShelfRequest> httpRequest = new HttpEntity<>(new HttpHeaders());
         // send request, and return response
         return new RestTemplate()
             .exchange(targetUrl, HttpMethod.DELETE, httpRequest, String.class)
