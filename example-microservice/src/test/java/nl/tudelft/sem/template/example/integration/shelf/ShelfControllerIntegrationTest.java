79c178177bebda5bc892e8550caf85d7a018f35d
Karol Sperczynski <k.sperczynski@student.tudelft.nl>
2024-01-18 01:09:45 +0100

    test: add integration test for removing book from bookshelf

--- example-microservice/src/test/java/nl/tudelft/sem/template/example/integration/shelf/ShelfControllerIntegrationTest.java
+++ example-microservice/src/test/java/nl/tudelft/sem/template/example/integration/shelf/ShelfControllerIntegrationTest.java
@@ -2,6 +2,7 @@ package nl.tudelft.sem.template.example.integration.shelf;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
 import static com.github.tomakehurst.wiremock.client.WireMock.configureFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.delete;
 import static com.github.tomakehurst.wiremock.client.WireMock.post;
 import static com.github.tomakehurst.wiremock.client.WireMock.urlEqualTo;
 import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.options;
@@ -13,15 +14,9 @@ import com.github.tomakehurst.wiremock.http.Body;
 import java.util.Optional;
 import nl.tudelft.sem.template.example.controllers.ShelfController;
 import nl.tudelft.sem.template.example.dtos.bookshelf.ManageBookShelfResponse200;
-import nl.tudelft.sem.template.example.modules.user.BannedType;
-import nl.tudelft.sem.template.example.modules.user.DetailType;
-import nl.tudelft.sem.template.example.modules.user.EmailType;
-import nl.tudelft.sem.template.example.modules.user.FollowingType;
-import nl.tudelft.sem.template.example.modules.user.PasswordType;
-import nl.tudelft.sem.template.example.modules.user.PrivacyType;
+import nl.tudelft.sem.template.example.modules.builders.UserBuilder;
+import nl.tudelft.sem.template.example.modules.builders.UserDirector;
 import nl.tudelft.sem.template.example.modules.user.User;
-import nl.tudelft.sem.template.example.modules.user.UserEnumType;
-import nl.tudelft.sem.template.example.modules.user.UsernameType;
 import nl.tudelft.sem.template.example.repositories.BookRepository;
 import nl.tudelft.sem.template.example.repositories.UserRepository;
 import nl.tudelft.sem.template.example.services.RestService;
@@ -48,6 +43,9 @@ public class ShelfControllerIntegrationTest {
     private ShelfController shelfController;
     private static WireMockServer wireMockServer;
 
+    private User correctUser;
+    private User bannedUser;
+
     @Mock
     UserRepository userRepository;
     @Mock
@@ -67,6 +65,15 @@ public class ShelfControllerIntegrationTest {
         // of the service, controller with the other team's endpoint
         this.shelfService = new ShelfService(userRepository, bookRepository);
         this.shelfController = new ShelfController(this.shelfService, new RestService());
+        // and build users
+        UserBuilder builder = new UserBuilder();
+        UserDirector director = new UserDirector(builder);
+
+        director.constructValidUser();
+        this.correctUser = builder.build();
+
+        director.constructBannedUser();
+        this.bannedUser = builder.build();
     }
 
     @AfterAll
@@ -79,19 +86,8 @@ public class ShelfControllerIntegrationTest {
         // set up mock DB -> both true!
         when(userRepository.existsById(123L)).thenReturn(true);
         when(bookRepository.existsById(5L)).thenReturn(true);
-        // make sure that the returned User is not banned
-        User exampleUser = new User(
-            new UsernameType("username"),
-            new EmailType("example@mail.com"),
-            new PasswordType("123HashedPassword"),
-            new BannedType(false),
-            new PrivacyType(false),
-            new UserEnumType("USER"),
-            new DetailType(),
-            new FollowingType()
-        );
         // configure mock response from DB
-        when(userRepository.findById(123L)).thenReturn(Optional.of(exampleUser));
+        when(userRepository.findById(123L)).thenReturn(Optional.of(correctUser));
         // set up wiremock to return 201 CREATED just like it would in a real scenario
         wireMockServer.stubFor(
             post(urlEqualTo("/bookshelf/2/book?userId=123&bookshelfId=2"))
@@ -103,23 +99,29 @@ public class ShelfControllerIntegrationTest {
     }
 
     @Test
+    public void returnsSuccessWhenCorrectlyRemoved() {
+        // set up mock DB -> both true!
+        when(userRepository.existsById(123L)).thenReturn(true);
+        when(bookRepository.existsById(5L)).thenReturn(true);
+        // configure mock response from DB
+        when(userRepository.findById(123L)).thenReturn(Optional.of(correctUser));
+        // set up wiremock to return 201 CREATED just like it would in a real scenario
+        wireMockServer.stubFor(
+            delete(urlEqualTo("/bookshelf/2/book?userId=123&books=5&bookshelfId=2"))
+                .willReturn(aResponse().withStatus(200))
+        );
+        // and check if the controller returns the correct response
+        assertEquals(shelfController.removeBookFromBookshelf(123L, 2L, 5L), ResponseEntity.status(
+            HttpStatus.OK).body(new ManageBookShelfResponse200(2L, 5L)));
+    }
+
+    @Test
     public void returns500IfBookshelfMicroserviceEncounteredError() {
         // set up mock DB -> both true!
         when(userRepository.existsById(123L)).thenReturn(true);
         when(bookRepository.existsById(5L)).thenReturn(true);
-        // make sure that the returned User is not banned
-        User exampleUser = new User(
-            new UsernameType("username"),
-            new EmailType("example@mail.com"),
-            new PasswordType("123HashedPassword"),
-            new BannedType(false),
-            new PrivacyType(false),
-            new UserEnumType("USER"),
-            new DetailType(),
-            new FollowingType()
-        );
         // configure mock response from DB
-        when(userRepository.findById(123L)).thenReturn(Optional.of(exampleUser));
+        when(userRepository.findById(123L)).thenReturn(Optional.of(correctUser));
         // set up wiremock to return 201 CREATED just like it would in a real scenario
         wireMockServer.stubFor(
             post(urlEqualTo("/bookshelf/2/book?userId=123&bookshelfId=2"))
@@ -134,34 +136,36 @@ public class ShelfControllerIntegrationTest {
     public void returns404IfUserNotFound() {
         when(userRepository.existsById(123L)).thenReturn(false);
         assertEquals(shelfController.addBookToBookshelf(123L, 2L, 5L), ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+        assertEquals(
+            shelfController.removeBookFromBookshelf(123L, 2L, 5L),
+            ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
     @Test
     public void returns404IfBookNotFound() {
         when(userRepository.existsById(123L)).thenReturn(true);
         when(bookRepository.existsById(5L)).thenReturn(false);
-        assertEquals(shelfController.addBookToBookshelf(123L, 2L, 5L), ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+        assertEquals(
+            shelfController.addBookToBookshelf(123L, 2L, 5L),
+            ResponseEntity.status(HttpStatus.NOT_FOUND).build());
+        assertEquals(
+            shelfController.removeBookFromBookshelf(123L, 2L, 5L),
+            ResponseEntity.status(HttpStatus.NOT_FOUND).build());
     }
 
     @Test
     public void returns403IfUserBanned() {
         when(userRepository.existsById(123L)).thenReturn(true);
         when(bookRepository.existsById(5L)).thenReturn(true);
-        User exampleUser = new User(
-            new UsernameType("username"),
-            new EmailType("example@mail.com"),
-            new PasswordType("123HashedPassword"),
-            new BannedType(true),
-            new PrivacyType(false),
-            new UserEnumType("USER"),
-            new DetailType(),
-            new FollowingType()
-        );
         // configure mock response from DB
-        when(userRepository.findById(123L)).thenReturn(Optional.of(exampleUser));
+        when(userRepository.findById(123L)).thenReturn(Optional.of(bannedUser));
         // and check
         assertEquals(
             shelfController.addBookToBookshelf(123L, 2L, 5L),
+            ResponseEntity.status(HttpStatus.FORBIDDEN).build()
+        );
+        assertEquals(
+            shelfController.removeBookFromBookshelf(123L, 2L, 5L),
             ResponseEntity.status(HttpStatus.FORBIDDEN).build()
         );
     }
