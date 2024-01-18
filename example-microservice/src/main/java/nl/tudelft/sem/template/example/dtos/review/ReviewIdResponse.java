e9bbce5ed5ed1ba9e5e4f07de6a71a767b4cfc0f
Eduard Alex Mihai <E.A.Mihai@student.tudelft.nl>
2024-01-18 02:13:07 +0100

    Successfully tested the delete reviews integration

---
+++ example-microservice/src/main/java/nl/tudelft/sem/template/example/dtos/review/ReviewIdResponse.java
@@ -0,0 +1,10 @@
+package nl.tudelft.sem.template.example.dtos.review;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ReviewIdResponse {
+    private Long reviewId;
+}
